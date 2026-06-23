4b2b369db987e4a05f8f2c6ed4468ef1b9835cbb
Toby Driscoll <driscoll@udel.edu>
2026-06-23 09:36:42 -0400

    fix allocation for newton and secant in case of maximum iterations

--- fncbook/tests/test_chapter4.py
+++ fncbook/tests/test_chapter4.py
@@ -39,3 +39,24 @@ def test_systems():
     assert isclose(nlfun(x[-1]), [0, 0, 0]).all()
     x = FNC.levenberg(nlfun, np.array([10.,-4,-3]))
     assert isclose(nlfun(x[-1]), [0, 0, 0]).all()
+
+def test_nlsq():
+    t = np.linspace(-1, 1, 40)
+    y = np.sin(np.exp(3 * t))
+    def nlfun(x):
+        f = x[0] * t**0 + x[1] * t + x[2] * np.sin(t)- y
+        return f
+    x = FNC.levenberg(nlfun, np.array([1., 1, 1]))
+    assert isclose(x[-1], [0.25941633, 2.9946118, -3.417461]).all()
+
+def test_newton_fail():
+    f = lambda x: np.exp(x) - x - 2
+    dfdx = lambda x: x    # wrong derivative to force failure
+    with pytest.warns(UserWarning):
+        FNC.newton(f, dfdx, 1.0)
+    
+
+def test_secant_fail():
+    f = lambda x: np.sqrt(np.abs(x))
+    with pytest.warns(UserWarning):
+        x = FNC.secant(f, 2, 1)
